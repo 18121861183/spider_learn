e61de9500ba7eed91aba9d444eb683e294d53f02
18121861183 <yaochen.zhao@colasoft.com.cn>
2018-09-06 10:56:54 +0800

    Python3.6实例

---
+++ unit3/03_request_timeout.py
@@ -0,0 +1,9 @@
+import socket
+import urllib.request
+
+try:
+    response = urllib.request.urlopen('http://httpbin.org/get', timeout=0.1)
+    print(response.read())
+except urllib.error.URLError as e:
+    if isinstance(e.reason, socket.timeout):
+        print('time out')
