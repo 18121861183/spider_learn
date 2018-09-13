edc114c8e2c67ae9c499b61a4c6fee84df477409
18121861183 <yaochen.zhao@colasoft.com.cn>
2018-09-13 10:07:51 +0800

    图片识别

---
+++ unit8_checkcode/01_picture_checkcode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@author: zyc
+@contact: yaochen.zhao@colasoft.com.cn
+@software: PyCharm
+@file: 01_picture_checkcode.py
+@time: 2018/9/13 09:52
+"""
+import tesserocr
+from PIL import Image
+
---
+++ unit8_checkcode/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@author: zyc
+@contact: yaochen.zhao@colasoft.com.cn
+@software: PyCharm
+@file: __init__.py.py
+@time: 2018/9/13 09:50
+"""
